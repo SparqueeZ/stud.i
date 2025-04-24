d156b4f57b4244b563eee7459307c9df88b26a00
Yuksss <rpetit.bach2025@esaip.com>
2025-04-24 15:36:36 +0200

    Création du front register client

--- front/src/assets/js/icons.js
+++ front/src/assets/js/icons.js
@@ -23,4 +23,10 @@ export const icons = [
       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></svg>
       `,
   },
+  {
+    name: 'user',
+    user: `
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
+      `,
+  },
 ]
--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -11,7 +11,16 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/login.vue'),
     },
+    {
+      path: '/register',
+      name: 'register',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/register.vue'),
+    },
   ],
-})
+}
+)
 
 export default router
